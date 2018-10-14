fe75e77be1fe48650006fd32ca382b910c102c6d
David Cheng <dc273@duke.edu>
2018-10-14 01:05:01 -0400

    Regis path

---
+++ HackDuke/HackDuke/ViewControllers/RegisterDonator.swift
@@ -0,0 +1,31 @@
+//
+//  RegisterDeliver.swift
+//  HackDuke
+//
+//  Created by David Cheng on 10/14/18.
+//  Copyright © 2018 CDEV. All rights reserved.
+//
+
+import Foundation
+import Eureka
+
+class RegisterDonator: FormViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        form +++ Section("Section1")
+            <<< TextRow(){ row in
+                row.title = "Text Row"
+                row.placeholder = "Enter text here"
+            }
+            <<< PhoneRow(){
+                $0.title = "Phone Row"
+                $0.placeholder = "And numbers here"
+            }
+            +++ Section("Section2")
+            <<< DateRow(){
+                $0.title = "Date Row"
+                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+        }
+    }
+}
