1709cb3bfac2aa5e33b102cb9c8ab38b10f17349
David Cheng <dc273@duke.edu>
2018-10-14 10:32:03 -0400

    deliverer home

---
+++ HackDuke/HackDuke/ViewControllers/DelivererHome.swift
@@ -0,0 +1,20 @@
+//
+//  DelivererHome.swift
+//  HackDuke
+//
+//  Created by David Cheng on 10/14/18.
+//  Copyright © 2018 CDEV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DelivererHome: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ HackDuke/HackDuke/ViewControllers/DelivererHomePC1.swift
@@ -0,0 +1,15 @@
+//
+//  DelivererHomePC1.swift
+//  HackDuke
+//
+//  Created by David Cheng on 10/14/18.
+//  Copyright © 2018 CDEV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DelivererHomePC1: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var availible: UILabel!
+}
