a8a637f4cb7d062f27877cae36649395b0330b15
David Cheng <dc273@duke.edu>
2018-10-14 12:50:08 -0400

    logic

---
+++ HackDuke/HackDuke/ViewControllers/DelivererHomePC2.swift
@@ -0,0 +1,16 @@
+//
+//  DelivererHomePC1.swift
+//  HackDuke
+//
+//  Created by David Cheng on 10/14/18.
+//  Copyright © 2018 CDEV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DelivererHomePC2: UITableViewCell {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var distance: UILabel!
+}
