c0f99457fe1127f41b9cd5ab24f51684a6492a4b
David Cheng <dc273@duke.edu>
2018-10-13 22:15:11 -0400

    alg

--- HackDuke/HackDuke/MyPlayground.playground/Contents.swift
+++ HackDuke/HackDuke/MyPlayground.playground/Contents.swift
@@ -13,15 +13,88 @@ var receipents = ["3039 E Cornwallis Rd, Research Triangle Park, NC 27709",
                   "966 High House Rd #124, Cary, NC 27513"
 ]
 
+var threshold = 5
+
+
+let myGroup = DispatchGroup()
+
+var original:Int = 0;
+var stage1 = [String:Int]();
+var stage2 = [String:Int]();
+var sum = [String:Int]();
+
+var parameters = [
+    "units": "imperial",
+    "key": "AIzaSyBg_NuQxdUewCsfbmJRUIpIj0qZKVnM24A",
+    "destinations": homeAddress,
+    "origins": store
+]
+
+myGroup.enter()
+Alamofire.request("https://maps.googleapis.com/maps/api/distancematrix/json", parameters: parameters)
+    .responseJSON { response in
+        if let json = response.result.value as? NSDictionary {
+            let rows = (((json["rows"]! as! [NSDictionary])[0]["elements"]!) as! [NSDictionary])[0]["duration"]!
+            let text = (rows as! NSDictionary)["text"]! as! String
+            let result = Int(text.replacingOccurrences(of: " mins", with: ""))!
+            original = result
+        }
+        myGroup.leave()
+}
+
 for receipent in receipents {
-    print(receipent)
+    parameters["origins"] = store
+    parameters["destinations"] = receipent
     
-    Alamofire.request("https://maps.googleapis.com/maps/api/distancematrix/json", parameters: ["foo": "bar"])
+    myGroup.enter()
+    Alamofire.request("https://maps.googleapis.com/maps/api/distancematrix/json", parameters: parameters)
         .responseJSON { response in
-            if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
+            if let json = response.result.value as? NSDictionary {
+                let rows = (((json["rows"]! as! [NSDictionary])[0]["elements"]!) as! [NSDictionary])[0]["duration"]!
+                let text = (rows as! NSDictionary)["text"]! as! String
+                let result = Int(text.replacingOccurrences(of: " mins", with: ""))!
+                stage1[receipent] = result
             }
+            myGroup.leave()
     }
+    
+    parameters["origins"] = receipent
+    parameters["destinations"] = homeAddress
+    myGroup.enter()
+    Alamofire.request("https://maps.googleapis.com/maps/api/distancematrix/json", parameters: parameters)
+        .responseJSON { response in
+            if let json = response.result.value as? NSDictionary {
+                let rows = (((json["rows"]! as! [NSDictionary])[0]["elements"]!) as! [NSDictionary])[0]["duration"]!
+                let text = (rows as! NSDictionary)["text"]! as! String
+                let result = Int(text.replacingOccurrences(of: " mins", with: "").replacingOccurrences(of: " min", with: ""))!
+                stage2[receipent] = result
+            }
+            myGroup.leave()
+    }
+    
     
 }
 
+extension Dictionary where Value: Equatable {
+    func someKey(forValue val: Value) -> Key? {
+        return first(where: { $1 == val })?.key
+    }
+}
+
+myGroup.notify(queue: .main) {
+    for receipent in receipents {
+        sum[receipent] = stage1[receipent]! + stage2[receipent]!
+    }
+    
+    let min = sum.values.min()!
+    let diff = min - original
+    
+    if diff <= threshold {
+        let address = sum.someKey(forValue: min)!
+        
+        print("Found address within threshold: " + address)
+    } else {
+        print("None within threshold")
+    }
+    
+}
