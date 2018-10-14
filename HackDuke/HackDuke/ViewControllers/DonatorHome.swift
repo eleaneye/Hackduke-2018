562a1a567d4ffc080175e0ad83bc967712e4dd7a
Eleane Ye <eleane.ye@gmail.com>
2018-10-14 12:49:01 -0400

    donatorhome

---
+++ HackDuke/HackDuke/ViewControllers/DonatorHome.swift
@@ -0,0 +1,28 @@
+//
+//  DonatorHome.swift
+//  HackDuke
+//
+//  Created by Eleane Ye on 10/14/18.
+//  Copyright © 2018 CDEV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class DonatorHome: UIViewController {
+    @IBOutlet weak var numMeals: UITextField!
+    
+    @IBOutlet weak var typeFood: UITextField!
+    
+    @IBAction func goDonate(_ sender: Any){
+        //let numMeals = numMeals.text
+       // let typeFood = typeFood.text
+    }
+    
+    @IBOutlet weak var goDonate: UIButton!
+    
+    @IBAction func register(_sender:Any){
+    }
+    
+}
